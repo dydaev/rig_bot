6893e3fdbdb55100cc20188db293e38043028872
dydaev <dydaev@gmail.com>
2021-12-15 00:15:42 +0200

    added snmp

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 var ping = require('ping');
+var snmp = require ("net-snmp");
 
-var hosts = ['192.168.0.11']//, '192.168.0.234', '192.168.0.123'];
+var hosts = ['192.168.0.249'];//rig host
+var snmpHost = '192.168.0.200';//rebooter
 
-var downTime = 1;//min
+var downTime = 0.5;//min
 var pingInterval = 5;//sec
 
 var counter = calcDownTime();
 var togglePing = true;
 
+var session = snmp.createSession (snmpHost, "your_password");
+
+//snmpInit();
+
 var interval = setInterval(function(hosts){
   
   hosts.forEach(function(host){
     ping.sys.probe(host, function(isAlive){
-      var dt = new Date();
-      var date = ("0" + dt.getDate()).slice(-2);
-      var month = ("0" + (dt.getMonth() + 1)).slice(-2);
-
-      // current year
-      var year = dt.getFullYear();
-
-      // current hours
-      var hours = dt.getHours();
-
-      // current minutes
-      var minutes = dt.getMinutes();
-
-      // current seconds
-      var seconds = dt.getSeconds();
-
-      //var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
       
       if (!isAlive) {
         if (togglePing) console.log(getTimeStr("full"), host, 'is DOWN');
@@ -41,18 +30,28 @@ var interval = setInterval(function(hosts){
         togglePing = true;
         counter = calcDownTime();
       }
-      //console.log(msg);
     });
   });
 
-  console.log(counter)
+  //console.log(counter)
 
   if (counter < 1) {
+    snmpReboot();
     //do something etc..reboot rig, send message
-		clearInterval(interval);
+		//clearInterval(interval);
+    counter = calcDownTime();
 	}
 
 }, pingInterval * 1000, hosts)
+
+if (interval) {
+  console.log(getTimeStr("full"), "started!");
+  ping.sys.probe(hosts[0], function(isAlive){
+    if (isAlive) {
+      console.log('ping to', hosts[0], 'good!')
+    }
+  });
+}
 
 function getTimeStr(quality = "full") {
   var dt = new Date();
@@ -78,4 +77,71 @@ function getTimeStr(quality = "full") {
 
 function calcDownTime() {
   return (downTime * 60) / pingInterval;
+}
+
+function snmpInit() {
+  var oids = ["1.3.6.1.4.1.35160.1.7.0"];
+  session.get (oids, function (error, varbinds) {
+      if (error) {
+          console.error (error);
+          process.exit(1)
+      } else {
+          for (var i = 0; i < varbinds.length; i++) {
+              if (snmp.isVarbindError (varbinds[i])) {
+                  console.error (snmp.varbindError (varbinds[i]));
+                  process.exit(1)
+              } else {
+                var snmpObjName = varbinds[i].value;
+                //console.log (varbinds[i].oid + " = " + varbinds[i].value);
+                console.log (snmpObjName + ' is initialised');
+             }
+         }
+     }
+      session.close ();
+  });
+}
+
+function setSnmpPowrPort(port, state) {
+  var varbinds = [
+    {
+        oid: "1.3.6.1.4.1.35160.1.11.1.4." + port,
+        type: snmp.ObjectType.Integer32,
+        value: state
+    }
+  ];
+  session.set (varbinds, function (error, varbinds) {
+    if (error) {
+        console.error (error.toString ());
+    } else {
+        for (var i = 0; i < varbinds.length; i++) {
+            console.log (getTimeStr("full"), varbinds[i].oid + " <- " + varbinds[i].value);
+
+            if (snmp.isVarbindError (varbinds[i]))
+                console.error (snmp.varbindError (varbinds[i]));
+            //else
+                //console.log (varbinds[i].oid + "|" + varbinds[i].value);
+        }
+    }
+  });
+
+}
+
+function snmpReboot(secTurnHold = 6) {
+  var upAfterDownSec = 10;
+
+  setSnmpPowrPort(1, 1);
+
+  setTimeout(function(){
+    setSnmpPowrPort(1, 0);
+  }, secTurnHold * 1000);
+
+  setTimeout(function(){
+    setSnmpPowrPort(1, 1);
+ 
+    setTimeout(function(){
+      setSnmpPowrPort(1, 0);
+    }, 1000);
+
+  }, (secTurnHold * 1000 + (upAfterDownSec * 1000)))
+
 }
